4d9158eb1603c76880855e501ed91b7af7009869
gaurav nv <gauravvenkat2k22@icloud.com>
2024-09-03 17:26:08 +0530

    feat: oops 1 class basics

--- oops_1.py
+++ oops_1.py
@@ -16,6 +16,14 @@ class Person:
     def getSalary(self):
         print(f"{self.name}'s income is {self._salary}")
 
+    @classmethod
+    def createPerson(cls, name, email, salary):
+        return cls(name, email, salary)
+    
+    @staticmethod
+    def addTwoNum(num1, num2):
+        return num1 + num2
+
 # class Person contains 2 properties name and age
 # name=ram, age=20, salary=10_000
 # note: 10_000 is similar to 10000
@@ -24,6 +32,15 @@ ram = Person("Ram", 20, 10_000)
 # methods of class can be accessed using . method
 ram.printPerson() # The person name is Ram and age 20
 ram.getSalary() # Ram's income is 10000
+
+# to access class with different instanciating methods using @classmethod
+Person.createPerson("Ravi",42,15_000).printPerson()
+# o/p: The person name is Ravi and age 42
+
+# to just use functions of class and dont use actual properties
+# use @staticmethod
+print(Person.addTwoNum(5, 6))
+# o/p: 11
 
 
 
